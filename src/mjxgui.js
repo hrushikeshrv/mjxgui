d671afbd3bf1c9f52e754add2c1719f46acfe1ef
Hrushikesh <hrushikeshvaidya2002@gmail.com>
2021-04-29 21:48:41 +0530

    Set up Cursor

--- src/mjxgui.js
+++ src/mjxgui.js
@@ -1,5 +1,55 @@
-class Cursor {
+const expressionList = [];
+const blockList = [];
 
+class Cursor {
+    constructor() {
+        this.currentBlock = 0;
+        this.currentExpression = 0;
+        this.x = 5;
+        this.y = 2;
+        this.height = 50;
+    }
+
+    isInBlock() {
+        return this.currentBlock !== null;
+    }
+
+    draw() {}
+
+    // Move the cursor to the next block in blockList
+    seekF() {}
+
+    // Move the cursor to the previous block in blockList
+    seekB() {}
+
+    // Delete the last character/block/expression
+    backspace() {
+        if (blockList.length === 0 || this.currentBlock === null) {
+            return;
+        }
+        // The block we are in will only contain text, because the cursor can only be at the "leaves" of the block "tree"
+        // If the block we are in is not empty we delete the last character from the block's latex
+        // Otherwise we check if the expression we are in is empty.
+        // If yes we delete the entire expression, else we do nothing
+        let cb = blockList[this.currentBlock];
+        if (!cb.isEmpty()) {
+            cb.latex = cb.latex.slice(0, -1);
+            this.seekB();
+        }
+        else {
+            if (expressionList[this.currentExpression].isEmpty()) {
+                expressionList.splice(this.currentExpression, 1);
+                this.seekB();
+            }
+        }
+    }
+
+    keyDown() {}
+
+    // Generate latex for all expressions in expressionList and quit the widget
+    write() {}
+
+    moveToBlock(block) {}
 }
 
 class GUISymbol {
@@ -38,8 +88,9 @@ class Block {
         else {
             let l = '';
             for (let exp of this.children) {
-                l += exp.generateLatex();
+                l += ' ' + exp.generateLatex();
             }
+            return l;
         }
     }
 
@@ -53,10 +104,9 @@ class Block {
 }
 
 class Expression {
-    constructor(name, height, width, blocks) {
-        this.name = name;
-        this.height = height;
+    constructor(width, height, blocks) {
         this.width = width;
+        this.height = height;
         this.blocks = blocks;
     }
 
@@ -75,5 +125,34 @@ class Expression {
 
     generateLatex() {
         throw new Error('The generateLatex function for this Expression has not been implemented yet.')
+    }
+}
+
+class NSqrt extends Expression {
+    constructor() {
+        const b1 = new Block();
+        const b2 = new Block();
+        // TODO - Push these two new blocks into the global blockList
+        //  at the appropriate position (Cursor index over blockList)
+        super(50, 30, [b1, b2]);
+    }
+
+    generateLatex() {
+        return `\\sqrt[${this.blocks[0].getLatex()}]{${this.blocks[1].getLatex()}}`
+    }
+}
+
+class Sum extends Expression {
+    constructor() {
+        const b1 = new Block();
+        const b2 = new Block();
+        const b3 = new Block();
+        // TODO - Push these two new blocks into the global blockList
+        //  at the appropriate position (Cursor index over blockList)
+        super(50, 50, [b1, b2, b3]);
+    }
+
+    generateLatex() {
+        return `\\sum_{${this.blocks[0].getLatex()}}^{${this.blocks[1].getLatex()}}{${this.blocks[2].getLatex()}}`;
     }
 }
