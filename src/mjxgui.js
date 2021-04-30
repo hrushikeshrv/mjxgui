b9c05a2ddcf6f8943a039c4119484d03bbafd2a9
Hrushikesh <hrushikeshvaidya2002@gmail.com>
2021-04-30 12:32:50 +0530

    creating Expressions from the console works

--- src/mjxgui.js
+++ src/mjxgui.js
@@ -3,8 +3,12 @@ const blockList = [];
 
 class Cursor {
     constructor() {
-        this.currentBlock = 0;
-        this.currentExpression = 0;
+        // currentBlock can take integer values as indices into blockList or null if we are not in a block (in between 2 Expressions/Symbols)
+        this.currentBlock = null;
+        // currentExpression can take integer values as indices into expressionList
+        // or half integer values to represent the cursor being in between two expressions/symbols
+        // When currentExpression is a half integer, currentBlock is null
+        this.currentExpression = -0.5;
         this.x = 5;
         this.y = 2;
         this.height = 50;
@@ -22,10 +26,44 @@ class Cursor {
     // Move the cursor to the previous block in blockList
     seekB() {}
 
+    // Update this.currentBlock everytime we delete an Expression/Symbol or move to a new Expression/Symbol
+    updateBlock(position = -1) {
+        let cb;
+        if (position === -1) cb = expressionList[this.currentExpression].blocks.slice(position)[0];
+        else cb = expressionList[this.currentExpression].blocks[position];
+
+        for (let block = 0; block < blockList.length; block++) {
+            if (blockList[block] === cb) {
+                this.currentBlock = block;
+                return;
+            }
+        }
+    }
+
+    seekBlock(newBlock) {
+        for (let block = 0; block < blockList.length; block++) {
+            if (blockList[block] === newBlock) {
+                this.currentBlock = block;
+                return;
+            }
+        }
+    }
+
     // Delete the last character/block/expression
     backspace() {
-        if (blockList.length === 0 || this.currentBlock === null) {
+        if (blockList.length === 0) {
             return;
+        }
+        // If the current block is null, we are in between two indices of the expressionList
+        // We remove the previous element in expressionList if it is empty, otherwise we do nothing
+        if (this.currentBlock === null) {
+            if (this.currentExpression < 1) return;
+            if (expressionList[parseInt(this.currentExpression)].isEmpty()) {
+                expressionList.splice(parseInt(this.currentExpression), 1);
+                this.currentExpression = parseInt(this.currentExpression);
+                this.updateBlock(-1);
+                this.seekB();
+            }
         }
         // The block we are in will only contain text, because the cursor can only be at the "leaves" of the block "tree"
         // If the block we are in is not empty we delete the last character from the block's latex
@@ -39,6 +77,8 @@ class Cursor {
         else {
             if (expressionList[this.currentExpression].isEmpty()) {
                 expressionList.splice(this.currentExpression, 1);
+                this.currentExpression--;
+                this.updateBlock(0);
                 this.seekB();
             }
         }
@@ -52,12 +92,31 @@ class Cursor {
     moveToBlock(block) {}
 }
 
+const mjxguiCursor = new Cursor();
+
 class GUISymbol {
     constructor(latex) {
         this.latex = latex;
     }
     draw() {
         throw new Error('The draw function for this GUISymbol has not been implemented yet.')
+    }
+
+    isEmpty() {
+        return true;
+    }
+
+    insertIntoExpressionList() {
+        // If the cursor is not in any block (and therefore on the top level), insert this Expression into the global expressionList
+        // else we are inside a block, so add this Symbol as a child of the block we are in and don't insert into the global expressionList
+        if (mjxguiCursor.currentBlock === null) {
+            expressionList.splice(Math.floor(mjxguiCursor.currentExpression)+1, 0, this);
+            mjxguiCursor.currentExpression = Math.floor(mjxguiCursor.currentExpression) + 1;
+            mjxguiCursor.updateBlock(0);
+        }
+        else {
+            blockList[mjxguiCursor.currentBlock].children.push(this);
+        }
     }
 
     generateLatex() {
@@ -68,6 +127,7 @@ class GUISymbol {
 class PM extends GUISymbol {
     constructor() {
         super('pm');
+        this.insertIntoExpressionList();
     }
 
     draw() {
@@ -114,6 +174,20 @@ class Expression {
         throw new Error('The draw function for this Expression has not been implemented yet.')
     }
 
+    insertIntoExpressionList() {
+        // If the cursor is not in any block (and therefore on the top level), insert this Expression into the global expressionList
+        // else we are inside a block, so add this Expresion as a child of the block we are in and don't insert into the global expressionList
+        if (mjxguiCursor.currentBlock === null) {
+            expressionList.splice(Math.floor(mjxguiCursor.currentExpression)+1, 0, this);
+            mjxguiCursor.currentExpression = Math.floor(mjxguiCursor.currentExpression) + 1;
+            mjxguiCursor.updateBlock(0);
+        }
+        else {
+            blockList[mjxguiCursor.currentBlock].children.push(this);
+            mjxguiCursor.seekBlock(this.blocks[0]);
+        }
+    }
+
     isEmpty() {
         for (let block of this.blocks) {
             if (!block.isEmpty()) {
@@ -132,9 +206,10 @@ class NSqrt extends Expression {
     constructor() {
         const b1 = new Block();
         const b2 = new Block();
-        // TODO - Push these two new blocks into the global blockList
-        //  at the appropriate position (Cursor index over blockList)
+        blockList.push(b1, b2);
+
         super(50, 30, [b1, b2]);
+        this.insertIntoExpressionList();
     }
 
     generateLatex() {
@@ -147,9 +222,10 @@ class Sum extends Expression {
         const b1 = new Block();
         const b2 = new Block();
         const b3 = new Block();
-        // TODO - Push these two new blocks into the global blockList
-        //  at the appropriate position (Cursor index over blockList)
+        blockList.push(b1, b2, b3);
+
         super(50, 50, [b1, b2, b3]);
+        this.insertIntoExpressionList();
     }
 
     generateLatex() {
