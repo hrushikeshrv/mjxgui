2d06567358356d8954447b5437e0a6969c505d93
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2021-10-06 19:19:16 +0530

    Remove webpack, add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            build: {
+                src: ['src/modules/expression-backend.js', 'src/modules/controller.js', 'src/modules/ui.js'],
+                dest: 'src/mjxgui.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! mjxgui <%= grunt.template.today("yyyy-mm-dd") %> | (C) Hrushikesh Vaidya | MIT License */',
+            },
+            build: {
+                src: 'src/mjxgui.js',
+                dest: 'src/mjxgui.min.js',
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+}
